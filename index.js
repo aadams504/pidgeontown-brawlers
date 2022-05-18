617b2d696a86ced5c88f7405b04131d80fb90891
Arianna <95667502+aadams504@users.noreply.github.com>
2022-05-18 14:09:06 -0700

    added game timer and end game functionality

--- index.js
+++ index.js
@@ -115,6 +115,34 @@ const keys = {
     }
 };
 
+const determineWinner = ({ player, enemy, timerId }) => {
+    clearTimeout(timerId);
+    document.querySelector('.display-text').style.display = 'flex';
+    if(player.health === enemy.health) {
+    document.querySelector('.display-text').innerHTML = 'Tie';
+    } else if (player.health > enemy.health) {
+        document.querySelector('.display-text').innerHTML = 'Player 1 Wins';
+    } else if (enemy.health > player.health) {
+        document.querySelector('.display-text').innerHTML = 'Player 2 Wins';
+    }
+}
+
+let timer = 60;
+let timerId;
+
+const decreaseTimer = () => {
+    if(timer > 0) {
+        timerId = setTimeout(decreaseTimer, 1000);
+        timer--;
+        document.getElementById('timer').innerHTML = timer;
+    }
+    if(timer === 0) {
+        document.querySelector('.display-text').style.display = 'flex';
+        determineWinner({ player, enemy, timerId });
+    }
+}
+
+decreaseTimer();
 
 const animate = () => {
     window.requestAnimationFrame(animate);
@@ -164,6 +192,12 @@ const animate = () => {
             enemy.isAttacking = false;
             player.health -= 20;
             document.getElementById('player-health').style.width = player.health + '%';
+    }
+
+    // end game based on health
+
+    if(enemy.health <= 0 || player.health <= 0) {
+        determineWinner({ player, enemy, timerId });
     }
 }
 
